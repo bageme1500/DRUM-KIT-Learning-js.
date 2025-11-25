14f5626e1facbc40723f420194aea28f3ff7d7c6
Boniphace <boniphacebageme06@gmail.com>
2025-11-25 20:46:52 +0300

    an attempt to implement the mouse click on each button to echo different sounds

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 numberOfButtons = document.querySelectorAll(".drum").length;
 
-for(var i=0; i< numberOfButtons; i++){
+for(let i=0; i< numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-  })
+    if(i >= 1 && i <= 4){
+      var audio = new Audio("sounds/tom-" + i + ".mp3");
+      audio.play();
+    }else if( i < 1){
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+    }else if (i===5){
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+    }else if (i===6){
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+    }
+  });
 }
