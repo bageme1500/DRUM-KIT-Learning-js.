2c39014bfdfb891c63af17780daa67e2095d20cb
Boniphace <boniphacebageme06@gmail.com>
2025-11-27 12:55:16 +0300

    modified to using switch statements instead of ifs

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ numberOfButtons = document.querySelectorAll(".drum").length;
 
 for(let i=0; i< numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    if(i >= 1 && i <= 4){
-      var audio = new Audio("sounds/tom-" + i + ".mp3");
-      audio.play();
-    }else if( i < 1){
-      var audio = new Audio("sounds/crash.mp3");
-      audio.play();
-    }else if (i===5){
-      var audio = new Audio("sounds/kick-bass.mp3");
-      audio.play();
-    }else if (i===6){
-      var audio = new Audio("sounds/snare.mp3");
-      audio.play();
-    }
+   var buttonClicked = this.innerHTML;
+
+   switch (buttonClicked) {
+     case "w":
+       new Audio("sounds/crash.mp3").play();
+       break;
+
+     case "a":
+       new Audio("sounds/tom-1.mp3").play();
+       break;
+
+     case "s":
+       new Audio("sounds/tom-2.mp3").play();
+       break;
+
+     case "d":
+       new Audio("sounds/tom-3.mp3").play();
+       break;
+
+     case "j":
+       new Audio("sounds/tom-4.mp3").play();
+       break;
+
+     case "k":
+       new Audio("sounds/kick-bass.mp3").play();
+       break;
+
+     case "l":
+       new Audio("sounds/snare.mp3").play();
+       break;
+
+     default:
+
+   }
   });
 }
